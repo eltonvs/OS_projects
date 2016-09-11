06b7e8ac1b07cd9b0323de79cc84773e04d76b6a
Elton Viana <eltonvs@outlook.com>
2016-09-11 11:41:36 -0300

    Update project

--- microproject/src/project.cpp
+++ microproject/src/project.cpp
@@ -1,7 +1,24 @@
 #include "BlackGPIO.h"
 #include <unistd.h>
 #include <cstdlib>
-#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <random>
+
+#define PATH_ADC "/sys/bus/iio/devices/iio:device0/in_voltage"
+
+int readAnalog(int number){
+   std::stringstream ss;
+   ss << PATH_ADC << number << "_raw";
+   std::fstream fs;
+   fs.open(ss.str().c_str(), std::fstream::in);
+   fs >> number;
+   fs.close();
+   return number;
+}
 
 int main(int argc, char *argv[]) {
     // Button
@@ -11,27 +28,45 @@ int main(int argc, char *argv[]) {
     BlackLib::BlackGPIO statusLed(BlackLib::GPIO_50,BlackLib::output, BlackLib::SecureMode);
 
     //Led RGB
-    BlackLib::BlackGPIO ledR1(BlackLib::GPIO_65,BlackLib::output, BlackLib::SecureMode);
-    BlackLib::BlackGPIO ledR2(BlackLib::GPIO_46,BlackLib::output, BlackLib::SecureMode);
-    BlackLib::BlackGPIO ledG(BlackLib::GPIO_26,BlackLib::output, BlackLib::SecureMode);
-    BlackLib::BlackGPIO ledB(BlackLib::GPIO_44,BlackLib::output, BlackLib::SecureMode);
+    BlackLib::BlackGPIO leds[3] = {
+        BlackLib::BlackGPIO(BlackLib::GPIO_46,BlackLib::output, BlackLib::SecureMode),
+        BlackLib::BlackGPIO(BlackLib::GPIO_26,BlackLib::output, BlackLib::SecureMode),
+        BlackLib::BlackGPIO(BlackLib::GPIO_44,BlackLib::output, BlackLib::SecureMode)
+    };
+
+    int ls[3] = {0, 1, 2};
+    unsigned int sleep_time = 10000;
 
     // Set everything to "default" values
     statusLed.setValue(BlackLib::high);
-    ledR1.setValue(BlackLib::high);
-    ledR2.setValue(BlackLib::high);
-    ledG.setValue(BlackLib::high);
-    ledB.setValue(BlackLib::high);
+    for (int i = 0; i < 3; i++)
+        leds[ls[i]].setValue(BlackLib::high);
 
-    printf("Executando o programa, clique no botão para parar!\n");
-    while (!button.getNumericValue()) {}
-    printf("Programa encerrado, desligando leds\n");
+    std::cout << "Executando o programa, pressione o botão para parar!\n";
+
+    while (!button.getNumericValue()) {
+        if (readAnalog(0) < 2000)
+            std::random_shuffle(ls, ls + 3);
+        sleep_time = (readAnalog(1) + 500)*100;
+        leds[ls[0]].setValue(BlackLib::high);
+        leds[ls[1]].setValue(BlackLib::low);
+        leds[ls[2]].setValue(BlackLib::low);
+        usleep(sleep_time);
+        leds[ls[0]].setValue(BlackLib::low);
+        leds[ls[1]].setValue(BlackLib::high);
+        leds[ls[2]].setValue(BlackLib::low);
+        usleep(sleep_time);
+        leds[ls[0]].setValue(BlackLib::low);
+        leds[ls[1]].setValue(BlackLib::low);
+        leds[ls[2]].setValue(BlackLib::high);
+        usleep(sleep_time);
+    }
+
+    std::cout << "Programa encerrado, desligando leds\n";
 
     // Reset everything
-    ledR1.setValue(BlackLib::low);
-    ledR2.setValue(BlackLib::low);
-    ledG.setValue(BlackLib::low);
-    ledB.setValue(BlackLib::low);
+    for (int i = 0; i < 3; i++)
+        leds[ls[i]].setValue(BlackLib::low);
     statusLed.setValue(BlackLib::low);
 
     return 0;
